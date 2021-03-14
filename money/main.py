ce1ca9598970cad9baec0e0929b1366912d2dcfb
Oleksiy Galimov <alexeigalimov@gmail.com>
2021-03-14 19:05:00 +0200

    new

--- money/main.py
+++ money/main.py
@@ -1,58 +1,57 @@
-import math
-
-
 class Money:
     def __init__(self, dollars, cents):
         self.dollars = dollars
-        self.cents = cents
-        self.total_money = self.dollars + self.cents
+        if 0 <= cents < 100:
+            self.cents = cents
+        else:
+            raise ValueError('Cents should be in range 0-100')
 
     def __str__(self):
-        return f'{self.total_money}$'
+        return f'{self.dollars}.{self.cents}$'
 
     def __add__(self, another_money):
-        sum_dollars = self.dollars + another_money.dollars
-        sum_cents = self.cents + another_money.cents
-        if sum_cents > 1:
-            sum_dollars += 1
-            sum_cents -= 1
-        return Money(sum_dollars, sum_cents)
+        total_cents = self.to_cents()
+        another_total_cents = another_money.to_cents()
+        cents_sum = total_cents + another_total_cents
+        return self.from_cents(cents_sum)
 
     def __sub__(self, another_money):
-        diff_dollars = self.dollars - another_money.dollars
-        diff_cents = self.cents - another_money.cents
-        if diff_cents < 0:
-            diff_dollars -= 1
-            diff_cents += 1
-        return Money(diff_dollars, diff_cents)
+        total_cents = self.to_cents()
+        another_total_cents = another_money.to_cents()
+        cents_diff = total_cents - another_total_cents
+        return self.from_cents(cents_diff)
 
-    def divide_sum(self, another_money, divider):
-        money_sum = self.total_money + another_money.total_money
-        sum_divide = money_sum / divider
-        return sum_divide
+    def __truediv__(self, divider):
+        total_cents = self.to_cents()
+        total_cents /= divider
+        return self.from_cents(total_cents)
 
-    def divide_diff(self, another_money, divider):
-        money_diff = self.total_money - another_money.total_money
-        diff_divide = money_diff / divider
-        return diff_divide
+    def __mul__(self, multiplier):
+        total_cents = self.to_cents()
+        total_cents *= multiplier
+        return self.from_cents(total_cents)
 
-    def multiply(self, multiplier):
-        return self.total_money * multiplier
+    def __gt__(self, another_money):
+        return self.to_cents() > another_money.to_cents()
 
-    def is_greater(self, another_money):
-        return self.total_money > another_money.total_money
+    def to_cents(self):
+        return self.dollars * 100 + self.cents
+
+    @classmethod
+    def from_cents(cls, cents):
+        dollars = cents // 100
+        cents = cents % 100
+        return cls(int(dollars), int(cents))
 
 
 if __name__ == '__main__':
     num = 2
-    money = Money(2, 0.5)
-    second_money = Money(2, 0.6)
+    money = Money(2, 50)
+    second_money = Money(2, 60)
     print(f'First value: {money}')
     print(f'Second value: {second_money}')
-    print(f'sum = {money.__add__(second_money)}')
-    print(f'diff = {money.__sub__(second_money)}')
-    print(f'division of sum = {money.divide_sum(money, num)}')
-    print(f'division of diff = {money.divide_diff(second_money, num)}')
-    print(f'multiply = {money.multiply(num)}')
-    print(f'is first number greater?\n{money.is_greater(second_money)}')
-
+    print(f'sum = {money + second_money}')
+    print(f'diff = {money - second_money}')
+    print(f'division = {money / num}')
+    print(f'multiply = {money * num}')
+    print(f'is first number greater?\n{money < second_money}')
